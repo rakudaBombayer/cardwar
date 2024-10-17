b28f7fd87d3646959eeb7719c9e8d4865291a89a
rakudaBombayer <113893608+rakudaBombayer@users.noreply.github.com>
2024-10-17 19:54:52 +0900

    初期の実装、戦争！

--- cardwar.php
+++ cardwar.php
@@ -1,6 +1,12 @@
 <?php
 
-echo "Hellow world";
+echo "戦争を開始します。";
+echo "カードが配られました";
+echo "戦争!";
+
+$ary = array("A", "K", "Q", "J","10","9","8","7","6","5","4","3","2");
+$key = array_rand($ary, 1);
+echo $ary[$key];
 
 
 ?>
