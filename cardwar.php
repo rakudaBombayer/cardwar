7dbbc9c07c64530d784b0160e501cbdb32f7f123
rakudaBombayer <113893608+rakudaBombayer@users.noreply.github.com>
2024-10-19 10:32:10 +0900

    配列から文字を取り出すことに成功2

--- cardwar.php
+++ cardwar.php
@@ -1,14 +1,32 @@
 <?php
 
 $text = "戦争を開始します。"."\n"."カードが配られました"."\n";
-
+$war = "戦争!"."\n";
 echo $text;
 
+echo $war;
 
 
-$ary = array("A", "K", "Q", "J","10","9","8","7","6","5","4","3","2");
-$key = array_rand($ary, 1);
-echo $ary[$key];
+//数字の配列
+$numbers = ["A", "K", "Q", "J","10","9","8","7","6","5","4","3","2"];
+$number = $numbers[array_rand($numbers)];
+
+// マークの配列
+$suits = ['ダイヤ', 'ハート', 'スペード', 'クローバー'];
+$suit = $suits[array_rand($suits)];
+
+$number = array_rand($numbers);
+
+echo $number;
+
+
+
+
+
+
+
+
+
 
 
 ?>
