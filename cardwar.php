65b14e1569073d2319df103b522afcfb79093d4a
rakudaBombayer <113893608+rakudaBombayer@users.noreply.github.com>
2024-10-19 10:45:39 +0900

    プレイヤー1とプレイヤー2で変数名を変えました。

--- cardwar.php
+++ cardwar.php
@@ -9,21 +9,17 @@ echo $war;
 
 //数字の配列
 $numbers = ["A", "K", "Q", "J","10","9","8","7","6","5","4","3","2"];
-$number = $numbers[array_rand($numbers)];
+$num1 = $numbers[array_rand($numbers)];
 
 // マークの配列
 $suits = ['ダイヤ', 'ハート', 'スペード', 'クローバー'];
 $suit = $suits[array_rand($suits)];
 
-echo $suit . "の" . $number;
+echo "プレイヤー1のカードは". $suit . "の" . $num1."です."."\n";
 
-
-
-
-
-
-
-
+$suit = $suits[array_rand($suits)];
+$num2 = $numbers[array_rand($numbers)];
+echo "プレイヤー2のカードは". $suit . "の" . $num2."です."."\n";
 
 
 
