ea10f5a3fe0d72bc1b68b816d6a146f90dd1e591
rakudaBombayer <113893608+rakudaBombayer@users.noreply.github.com>
2024-10-20 19:10:13 +0900

    マークと数字を引き分け時に聞く処理を追加しました。

--- try.php
+++ try.php
@@ -24,13 +24,34 @@ class Player {
   echo "戦争を終了します。\n";
     } else {
       echo "引き分けです!\n";
-      // $this->battle($player1, $player2);
+      do {
+        $newSuit1 = readline("プレイヤー1、新しいカードのマークを入力してください(ハート/ダイヤ/クラブ/スペード): ");
+        $newNumber1 = readline("プレイヤー1、新しいカードの数字を入力してください(1-13): ");
+        $newSuit2 = readline("プレイヤー2、新しいカードのマークを入力してください(ハート/ダイヤ/クラブ/スペード): ");
+        $newNumber2 = readline("プレイヤー2、新しいカードの数字を入力してください(1-13): ");
+
+        // 入力値の検証
+        if (!is_numeric($newNumber1) || $newNumber1 < 1 || $newNumber1 > 13 ||
+            !is_numeric($newNumber2) || $newNumber2 < 1 || $newNumber2 > 13 ||
+            !in_array($newSuit1, ['ハート', 'ダイヤ', 'クラブ', 'スペード']) ||
+            !in_array($newSuit2, ['ハート', 'ダイヤ', 'クラブ', 'スペード'])) {
+          echo "カードの数字は1から13までの整数、マークはハート、ダイヤ、クラブ、スペードのいずれかを入力してください。\n";
+        } else {
+          $player1->number = $newNumber1;
+          $player1->suit = $newSuit1;
+          $player2->number = $newNumber2;
+          $player2->suit = $newSuit2;
+          break;
+        }
+      } while (true);
+      // 再度バトル
+      $this->battle($player1, $player2);
     }
   }
 }
   // プレイヤーの作成
   $player1 = new Player("プレイヤー1", "ハート", 13);
-  $player2 = new Player("プレイヤー2", "スペード", 12);
+  $player2 = new Player("プレイヤー2", "スペード", 13);
 
   // ゲームの開始
   echo "戦争を開始します。"."\n";
