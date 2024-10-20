0562765d2dcbea46775ce4b320e7149b2e89720d
rakudaBombayer <113893608+rakudaBombayer@users.noreply.github.com>
2024-10-20 18:40:38 +0900

    なんとか提出できる形になりそうだ

--- try.php
+++ try.php
@@ -12,15 +12,28 @@ class Player {
   }
 
   class Game {
-  public function battle(Player $player) {
-  echo $player->name . "のカードは" . $player->suit . "の" . $player->number . "です。";
+  public function battle(Player $player1, Player $player2) {
+  echo $player1->name . "のカードは" . $player1->suit . "の" . $player1->number . "です。"."\n";
+  echo $player2->name . "のカードは" . $player2->suit . "の" . $player2->number . "です。"."\n";
+
+    
+  if ($player1->number > $player2->number) {
+    echo $player1->name . "の勝ちです!\n";
+  } else if ($player1->number < $player2->number){
+  echo $player2->name . "の勝ちです！\n";
+    } else {
+      echo "引き分けです!\n";
     }
   }
-
+}
   // プレイヤーの作成
-  $player1 = new Player("Alice", "ハート", 13);
+  $player1 = new Player("プレイヤー1", "ハート", 13);
+  $player2 = new Player("プレイヤー2", "スペード", 13);
 
   // ゲームの開始
+  echo "戦争を開始します。"."\n";
+  echo "カードが配られました。"."\n";
   $game = new Game();
-  $game->battle($player1);
+  $game->battle($player1, $player2);
+ 
   ?>
